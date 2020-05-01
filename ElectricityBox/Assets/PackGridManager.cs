65c3586f87d62623db52c783b40219fd7e8dc7b8
Nicholas Fuller <NCreation.Fuller@gmail.com>
2020-05-01 14:06:07 -0700

    scripts

--- ElectricityBox/Assets/PackGridManager.cs
+++ ElectricityBox/Assets/PackGridManager.cs
@@ -1,18 +1,135 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class PackGridManager : MonoBehaviour
+public class GridObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public const int INVALIDID = -1;
+    private static int IDCounter = 0;
+    public int ID { get; }
+
+    public int IntendedX { get; set; }
+    public int IntendedY { get; set; }
+
+    GridObject()
     {
-        
+        ID = ++IDCounter;
     }
 
-    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+}
+
+public class PackGridManager : MonoBehaviour 
+{
+    public const int GRIDW = 3;
+    public const int GRIDH = 10;
+
+    private class TileMap
+    {
+        private readonly Tile[,] tiles = new Tile[GRIDW, GRIDH];
+
+        public TileMap()
+        {
+            for (var x = 0; x < tiles.GetLength(0); x++)
+            {
+                for (var y = 0; y < tiles.GetLength(1); y++)
+                {
+                    tiles[x,y] = new Tile();
+                    tiles[x,y].SetEmpty();
+                }
+            }
+        }
+
+        public IEnumerable<Tile> IterateTiles()
+        {
+            for (var x = 0; x < tiles.GetLength(0); x++)
+            {
+                for (var y = 0; y < tiles.GetLength(1); y++)
+                {
+                    yield return tiles[x, y];
+                }
+            }
+        }
+
+        public void DebugLog()
+        {
+            StringBuilder ss = new StringBuilder();
+            for (var y = 0; y < tiles.GetLength(1); y++)
+            {
+                for (var x = 0; x < tiles.GetLength(0); x++)
+                {
+                    ss.Append($"{tiles[x, y].Type}");
+                }
+
+                ss.AppendLine();
+            }
+            Debug.Log(ss.ToString());
+        }
+
+        public bool SomethingAt(int x, int y)
+        {
+            return tiles[x, y].Type != TileType.EMPTY;
+        }
+
+        public void SetObjectAt(int x, int y, int id)
+        {
+            if (SomethingAt(x, y))
+                Debug.LogError($"Setting an object at {x,y} while there's something there!");
+            tiles[x,y].SetGridObject(id);
+        }
+    }
+    private readonly TileMap map = new TileMap();
+
+    public enum TileType
+    {
+        EMPTY,
+        BLOCKED,
+        GRIDOBJECT,
+    }
+                
+    public class Tile
+    {
+        public TileType Type;
+        public int ID;
+
+        public void SetBlocked()
+        {
+            Type = TileType.BLOCKED;
+            ID = GridObject.INVALIDID;
+        }
+        public void SetEmpty()
+        {
+            Type = TileType.EMPTY;
+            ID = GridObject.INVALIDID;
+        }
+        public void SetGridObject(int id)
+        {
+            Type = TileType.GRIDOBJECT;
+            ID = id;
+        }
+    }
+
+    public void AddObjectToGrid(GridObject obj, int x, int y)
+    {
+        map.SetObjectAt(x, y, obj.ID);
+    }
+
+    void Start()
+    {
+        foreach (var iterateTile in map.IterateTiles())
+        {
+            iterateTile.SetBlocked();
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            map.DebugLog();
+        }
     }
 }
---
+++ ElectricityBox/Assets/Scripts/BehaviourSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviourSingleton<T> : MonoBehaviour where T : class {
+
+    public static T obj { get { return _obj as T; } }
+    private static object _obj;
+
+	// Use this for initialization
+	public virtual void Awake()
+	{
+	    if (_obj != null)
+	        Debug.LogError("Assigning singleton twice: " + typeof (T).Name);
+        _obj = this;
+	}
+}
