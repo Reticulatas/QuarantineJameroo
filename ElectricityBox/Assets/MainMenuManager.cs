9ba04921ddc7f9d3bb6954c8024e0aaf04412c96
Nicholas Fuller <NCreation.Fuller@gmail.com>
2020-05-02 13:22:10 -0700

    .gitignore

---
+++ ElectricityBox/Assets/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Animator Animator;
+
+    private bool starting = false;
+
+    void Update()
+    {
+        if (starting)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Animator.SetTrigger("Start");
+            starting = true;
+        }
+    }
+}
